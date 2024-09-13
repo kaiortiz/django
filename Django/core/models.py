9da3d8dff2243979e348d6c76fd12442672bce4f
RODRIGO-BRAV0 <rodr.bravom@duocuc.cl>
2024-09-12 22:33:08 -0300

    conexion listado de categorias en index con oracle

--- Django/core/models.py
+++ Django/core/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+#MODELO PARA CATEGORIA
+class Categoria(models.Model):
+    id_categoria = models.IntegerField(primary_key=True,verbose_name='Id de categoria')
+    nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre de la categoria')
+    
+    def __str__(self) :
+        return self.nombreCategoria
--- Django/core/views.py
+++ Django/core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Categoria
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'core/index.html')
+    categoria = Categoria.objects.all()
+    datos = {
+        'categoria': categoria
+    }
+    return render(request, 'core/index.html',datos)
+
+    
