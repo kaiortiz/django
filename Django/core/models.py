599c7ada0669d4461529e1df698a769e4f7bb10a
kai <k.ortiz2@duocuc.cl>
2024-09-16 19:06:26 -0300

    correcion models y nueva carpeta modelo relacional

--- Django/core/models.py
+++ Django/core/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 #MODELO PARA CATEGORIA
 class Categoria(models.Model):
-    id_categoria = models.IntegerField(primary_key=True,verbose_name='Id de categoria')
+    id_categoria = models.IntegerField(primary_key=True,max_length=10, verbose_name='Id de categoria')
     nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre de la categoria')
     
     def __str__(self) :
